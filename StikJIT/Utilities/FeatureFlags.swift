07542195c49f0fd9dc7437d00699dc48507f53c0
Jamison Smith <barstaxjolster@gmail.com>
2026-01-03 20:05:40 -0800

    Enable Mini Tools feature in FeatureFlags

--- StikJIT/Utilities/FeatureFlags.swift
+++ StikJIT/Utilities/FeatureFlags.swift
@@ -8,5 +8,5 @@ enum FeatureFlags {
     /// Forces the Theme Expansion to be unlocked and visible everywhere.
     static let alwaysUnlockThemeExpansion = true
     /// Controls visibility of the Mini Tools tab and related UI.
-    static let isMiniToolsEnabled = false
+    static let isMiniToolsEnabled = true
 }
